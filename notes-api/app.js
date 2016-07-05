cadef4f7f2564ce2d972b7816c98fde6b87f1b61
Silva Barros <asbarros@indra.es>
2016-07-05 13:14:29 -0300

    enabled CORS

--- notes-api/app.js
+++ notes-api/app.js
@@ -22,9 +22,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-Auth-Token');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-Auth-Token');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE');
     next();
 });
 
