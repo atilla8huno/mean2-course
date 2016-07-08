662b725086bb8b488fe3e01fa55659dc1dae290a
Átilla Barros <atilla8huno@atilla8huno.local>
2016-07-08 14:06:12 -0300

    added connection to mongodb server;

---
+++ notes-api/connection/index.js
@@ -0,0 +1,21 @@
+/**
+ * Created by atilla8huno on 08/07/16.
+ */
+var mongoose = require('mongoose');
+var mongodb = require('./mongodb');
+
+const URL = '' +
+    mongodb.serverName + '://' +
+    mongodb.username + ':' +
+    mongodb.password + '@' +
+    mongodb.dns + ':' +
+    mongodb.port + '/' +
+    mongodb.database;
+
+var connection = {
+    connect: function () {
+        mongoose.connect(URL);
+    }
+};
+
+module.exports = connection;
