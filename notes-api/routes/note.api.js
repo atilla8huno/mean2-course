26fedd2a5b4a793ab87df3c197c671077718bec8
Silva Barros <asbarros@indra.es>
2016-07-15 16:03:56 -0300

    added userEmail in every request;

--- notes-api/routes/note.api.js
+++ notes-api/routes/note.api.js
@@ -5,27 +5,18 @@ var express = require('express');
 var router = express.Router();
 var NoteService = require('../services/note.service');
 var Note = require('../model/note');
-var firebase = require('firebase');
-
-// Firebase setup
-firebase.initializeApp({
-    serviceAccount: "./connection/firebase.json",
-    authDomain: "mean-course-2.firebaseapp.com",
-    databaseURL: "https://mean-course-2.firebaseio.com"
-});
-
-var auth = firebase.auth();
+var firebaseService = require('../services/firebase.service');
 
 router.use('/', function (req, res, next) {
     if (req.method === 'OPTIONS') {
         next();
     } else {
-        auth.verifyIdToken(req.get('X-Auth-Token'))
-            .then(function() {
-                next();
-            }, function(error) {
-                res.status(401).send(error);
-            });
+        firebaseService.validateToken(req.get('X-Auth-Token')).then(function (user) {
+            req.userEmail = user.email;
+            next();
+        }, function (error) {
+            res.status(401).send(error);
+        });
     }
 });
 
@@ -43,62 +34,50 @@ router.get('/by-id/:id', function (req, res, next) {
 });
 
 router.get('/all', function (req, res, next) {
-    var token = req.get('X-Auth-Token');
+    NoteService.findAll()
+        .then(function (docs) {
+            console.log(docs);
 
-    auth.verifyIdToken(token).then(function(user) {
-        NoteService.findAll()
-            .then(function (docs) {
-                console.log(docs);
-
-                res.status(200).json(docs);
-            }, function (error) {
-                res.status(404).send(error);
-            });
-    });
+            res.status(200).json(docs);
+        }, function (error) {
+            res.status(404).send(error);
+        });
 });
 
 router.post('/create', function (req, res, next) {
-    var token = req.get('X-Auth-Token');
-
-    auth.verifyIdToken(token).then(function(user) {
-        var note = new Note({
-            title: req.body.title,
-            description: req.body.description,
-            userEmail: user.email
-        });
-
-        NoteService.create(note)
-            .then(function (doc) {
-                console.log(doc);
-
-                res.header('Location', '/note/id/' + doc._id);
-                res.status(201).json(doc);
-            }, function (error) {
-                res.status(404).send(error);
-            });
+    var note = new Note({
+        title: req.body.title,
+        description: req.body.description,
+        userEmail: req.userEmail
     });
+
+    NoteService.create(note)
+        .then(function (doc) {
+            console.log(doc);
+
+            res.header('Location', '/note/by-id/' + doc._id);
+            res.status(201).json(doc);
+        }, function (error) {
+            res.status(404).send(error);
+        });
 });
 
 router.put('/update', function (req, res, next) {
-    var token = req.get('X-Auth-Token');
-
-    auth.verifyIdToken(token).then(function(user) {
-        var note = new Note({
-            _id: req.body._id,
-            title: req.body.title,
-            description: req.body.description
-        });
-
-        NoteService.update(note)
-            .then(function (doc) {
-                // the OLD doc
-                console.log(doc);
-
-                res.status(200).json(doc);
-            }, function (error) {
-                res.status(404).send(error);
-            });
+    var note = new Note({
+        _id: req.body._id,
+        title: req.body.title,
+        description: req.body.description
     });
+
+    NoteService.update(note)
+        .then(function (doc) {
+            // the OLD doc
+            console.log(doc);
+
+            res.status(200).json(doc);
+        }, function (error) {
+            res.status(404).send(error);
+        });
 });
 
 router.delete('/delete/:id', function (req, res, next) {
---
+++ notes-api/services/firebase.service.js
@@ -0,0 +1,31 @@
+/**
+ * Created by asbarros on 15/07/2016.
+ */
+var firebase = require('firebase');
+var Promise = require('promise');
+
+// Firebase setup
+firebase.initializeApp({
+    serviceAccount: "./connection/firebase.json",
+    authDomain: "mean-course-2.firebaseapp.com",
+    databaseURL: "https://mean-course-2.firebaseio.com"
+});
+
+var auth = firebase.auth();
+
+var firebaseService = {
+    validateToken: validateToken
+};
+
+function validateToken(token) {
+    return new Promise(function (resolve, reject) {
+        auth.verifyIdToken(token)
+            .then(function(user) {
+                resolve(user);
+            }, function(error) {
+                reject(error);
+            });
+    });
+}
+
+module.exports = firebaseService;
